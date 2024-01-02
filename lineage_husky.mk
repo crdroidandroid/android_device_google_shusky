019a48cdd458dcaa1e326cb3c8905146fd35b0a8
Ionut Gherman <ghermanionut96@gmail.com>
2024-01-02 02:14:18 +0200

    shusky: enable UDFPS customizations Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- lineage_husky.mk
+++ lineage_husky.mk
@@ -11,6 +11,8 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_IS_PIXEL := true
 TARGET_IS_PIXEL_8 := true
 WITH_GMS := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
